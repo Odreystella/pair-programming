b9e9b42b61846b0f93708f761111a761caef3903
heejung-gjt <hee_9803@naver.com>
2021-03-23 16:15:20 +0900

    fix: ex4

--- ex4.js
+++ ex4.js
@@ -1,21 +1,19 @@
 let todos = [
-    { id: 3, content: 'HTML', completed: false },
-    { id: 2, content: 'CSS', completed: true },
-    { id: 1, content: 'Javascript', completed: false }
-  ];
-  
-  function addTodo(newTodo) {
-    todos = [newTodo, ...todos];
-  }
-  
-  addTodo({ id: 4, content: 'Test', completed: false });
-  
-  console.log(todos);
-  /*
-  [
-    { id: 4, content: 'Test', completed: false },
-    { id: 3, content: 'HTML', completed: false },
-    { id: 2, content: 'CSS', completed: true },
-    { id: 1, content: 'Javascript', completed: false }
-  ]
-  */
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+const addTodo = (newTodo) => {todos = [newTodo, ...todos]};
+
+addTodo({ id: 4, content: 'Test', completed: false });
+console.log(todos);
+
+/*
+[
+  { id: 4, content: 'Test', completed: false },
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+]
+*/
