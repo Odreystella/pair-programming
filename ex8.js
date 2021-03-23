4b0f0355ff75550af9556bfefb9d6a855bc256ce
heejung-gjt <hee_9803@naver.com>
2021-03-23 16:17:53 +0900

    feat: resolve ex8

---
+++ ex8.js
@@ -0,0 +1,14 @@
+let count = 0;
+let todos = [
+    { id: 3, content: 'HTML', completed: false },
+    { id: 2, content: 'CSS', completed: true },
+    { id: 1, content: 'Javascript', completed: false }
+  ];
+
+  
+const countCompletedTodos = () => {
+  todos.filter(todo => { todo.completed === true ? count++ : count})
+  return count;
+}
+  
+console.log(countCompletedTodos()); // 1
