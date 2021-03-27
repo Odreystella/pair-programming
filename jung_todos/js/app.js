a2961984574e10a26c5894bd85cec3a4fcde965c
heejung-gjt <hee_9803@naver.com>
2021-03-27 15:58:33 +0900

    fix: add count Todo
    - 초기 비어있는 todo-list
    - checkbox bool타입 변경
    - checkbox completed: true인 todo list 개수 카운트

--- jung_todos/js/app.js
+++ jung_todos/js/app.js
@@ -4,7 +4,8 @@ const $todos = document.querySelector('.todos');
 const $inputTodo = document.querySelector('.input-todo');
 const $form = document.querySelector('.form');
 const $activeTodos = document.querySelector('.active-todos');
-
+const $clearBtn = document.querySelector('.btn');
+let clearCnt = 0;
 
 const render = () => {
     $todos.innerHTML = todos.map(({id,content,completed}) => {
@@ -51,6 +52,32 @@ $todos.onclick = e => {
   if(!e.target.classList.contains('remove-todo')) return;
   const id = e.target.parentNode.id;
   removeTodo(id);
+}
+
+// click 되어 있는 리스트 Clearbtn누르면 제거되는 기능을 가진 함수 
+const clearTodo = () => {
+  todos = todos.filter(todo => todo.completed !== true)
+  render();
+}
+
+// clearbtn누르면 완료된 todo의 개수 카운트되어 기록
+$clearBtn.onclick = () => {
+  const $completedTodos = document.querySelector('.completed-todos');
+  clearCnt += todos.filter(todo => todo.completed === true).length;
+  $completedTodos.textContent = clearCnt;
+  clearTodo();
+}
+
+// checkbox 클릭하면 true , 클릭해제하면 false
+const chkBoxTodo = chkId => {
+  if (!todos.id === chkId) return;
+  todos = todos.map(todo => todo.id === +chkId ? {...todo, completed: !todo.completed} : todo)
+}
+
+// checkbox 클릭시 checkbox부모의 id 출력
+$todos.onchange = e => {
+  const chkId = e.target.parentNode.id;
+  chkBoxTodo(chkId);
 }
 
 
