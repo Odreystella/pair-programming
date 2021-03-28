196be0e420f9d266e45c830ce501c3bbd9c28b52
Oderystella <ddott59@gmail.com>
2021-03-28 21:04:39 +0900

    feat: add addTodo() and form.onsubmit()

--- jiyun_totos/js/app.js
+++ jiyun_totos/js/app.js
@@ -1,6 +1,13 @@
 // State
 let todos = [];
-const $todos = document.querySelector('.todos');
+const $todos = document.querySelector('.todos'); // ul 요소 노드 가리킴
+const $inputTodo = document.querySelector('.input-todo'); // input 요소 노드 가리킴
+const $form = document.querySelector('.form'); // form 요소 노드 가리킴
+
+// id생성 함수
+const generateId = () => {
+  return Math.max(...todos.map((todo) => todo.id), 0) + 1;
+};
 
 // 데이터 렌더링 함수, HTML만들고 DOM에 반영
 const render = () => {
@@ -11,6 +18,7 @@ const render = () => {
     <i class="remove-todo far fa-times-circle"></i>
     </li>`
   }).join('');
+  console.log(todos)
 };
 
 // 데이터 초기화 함수, id는 내림차순 정렬
@@ -20,8 +28,29 @@ const getTodo = () => {
       { id: 2, content: 'CSS', completed: true },
       { id: 3, content: 'Javascript', completed: false }
     ].sort(( todo1, todo2 ) => todo2.id - todo1.id);
-    console.log(todos)
-    render();
+  render();
 };
 
+// DOM이 완성되면 getTodo 호출
 document.addEventListener('DOMContentLoaded', getTodo);
+
+// 신규데이터 추가 함수
+const addTodo = (content) => {
+  todos = [{id: generateId(), content, completed: false }, ...todos];
+  render();
+};
+
+// input data 삭제 및 포커스
+$form.onsubmit = e => {
+  e.preventDefault();
+
+  const content = $inputTodo.value; // input data 가리킴
+  $inputTodo.value = '';
+  $inputTodo.focus();
+  
+  addTodo(content); 
+};
+
+
+
+
